f57c1f60777e2f9205f0b5bd4784c9a9b29ce6a3
Chernovasilenko <a.chernovasilenko@yandex.ru>
2023-12-26 13:17:25 +0500

    add views: genre, categories, title

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.filters import SearchFilter
+
+from reviews.models import Category, Genre, Title
+from serializers import CategorySerializer, GenreSerializer, TitleSerializer
 
 
+class GenreCategoryMixin(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Миксин для жанров и категорий."""
+
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+    # добавить пермишен - админ или только чтение
+    # permission_classes = (IsAdminOrReadOnly,)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для произведений.
+    """
+    # добавить в кверисет среднее значение через annotate
+    queryset = Title.objects.annotate(rating=7)  # вместо 7 - AVG
+    serializer_class = TitleSerializer
+    filterset_fields = ('name',)
+    ordering = ('name',)
+    # добавить пермишен: админ или только чтение
+    # permission_classes = (IsAdminOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return TitleSerializer
+        return TitleSerializer
+
+
+class GenreViewSet(GenreCategoryMixin):
+    """
+    Вьюсет для жанров.
+    """
+
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CategoriesViewSet(GenreCategoryMixin):
+    """
+    Вьюсет для категорий.
+    """
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,44 +1,46 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
+
+from core import constants
 
 
-class Genre(models.Model):
-    """Модель для жанра."""
+class AbstractModelGenreCategory(models.Model):
+    """Абстрактная модель для жанров и категорий."""
 
     name = models.CharField(
         verbose_name='Название',
-        max_length=256,
+        max_length=constants.MAX_LENGHT_CHAR_FIELD,
     )
     slug = models.SlugField(
-        max_length=256,
+        max_length=constants.MAX_LENGHT_SLUG_FIELD,
         unique=True,
     )
 
     class Meta:
-        verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'
+        abstract = True
+        ordering = ('name',)
 
     def __str__(self):
-        return self.name
+        return self.name[:constants.MAX_TITLE_LENGTH]
+
+
+class Genre(AbstractModelGenreCategory):
+    """Модель для жанров."""
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        default_related_name = 'genres'
 
 
 class Category(models.Model):
     """Модель для категорий."""
 
-    name = models.CharField(
-        verbose_name='Название',
-        max_length=256,
-    )
-    slug = models.SlugField(
-        max_length=256,
-        unique=True,
-    )
-
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-
-    def __str__(self):
-        return self.name
+        default_related_name = 'categories'
 
 
 class Title(models.Model):
@@ -46,10 +48,20 @@ class Title(models.Model):
 
     name = models.CharField(
         verbose_name='Название',
-        max_length=256,
+        max_length=constants.MAX_LENGHT_CHAR_FIELD,
     )
     year = models.PositiveSmallIntegerField(
         verbose_name='Год',
+        validators=[
+            MinValueValidator(
+                limit_value=constants.MIN_YEAR,
+                message='Год выпуска не может быть меньше или равен нулю!'
+            ),
+            MaxValueValidator(
+                limit_value=timezone.now().year,
+                message='Год выпуска не может быть больше текущего!'
+            )
+        ]
     )
     description = models.TextField(
         verbose_name='Описание',
@@ -69,8 +81,9 @@ class Title(models.Model):
     )
 
     class Meta:
+        ordering = ('name',)
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
 
     def __str__(self):
-        return self.name
+        return self.name[:constants.MAX_TITLE_LENGTH]
