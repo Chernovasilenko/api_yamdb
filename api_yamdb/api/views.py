4a0ca26ef04135aa38f6b9ba4010d7c402c06d0f
Espo <notjustespo@yandex.ru>
2024-01-09 16:47:50 +0300

    Фикс наименований ролей

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,5 @@
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.contib.auth.tokens import PasswordResetTokenGenerator
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
